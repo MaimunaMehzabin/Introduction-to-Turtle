5bb7d79700e45b630b2cdb53122af33fc2e89100
Maimuna <knbari88@gmail.com>
2025-02-12 22:40:21 +0600

    asd

---
+++ Square.py
@@ -0,0 +1,21 @@
+import turtle
+
+turtle.Screen().bgcolor("lavender")
+turtle.Screen().setup(300,400)
+
+polygon = turtle.Turtle()
+
+    
+polygon.forward(70)
+polygon.right(90)
+
+polygon.forward(70)
+polygon.right(90)
+
+polygon.forward(70)
+polygon.right(90)
+
+polygon.forward(70)
+polygon.right(90)
+    
+turtle.done()
